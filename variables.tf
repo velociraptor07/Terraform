72ad990f18a54cd10a5984bbbeba91bed609bd19
velociraptor07 <sachdevyash61@gmail.com>
2025-03-18 15:45:28 -0400

    sus

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+  sensitive = true
+}
+
+variable "tenant_id" {
+  type = string
+}
