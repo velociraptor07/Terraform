adde599d9488f6ec98d0fb822beae56d648a4aab
velociraptor07 <sachdevyash61@gmail.com>
2025-03-18 16:21:16 -0400

    sus7

--- variables.tf
+++ variables.tf
@@ -1,16 +1,19 @@
 variable "subscription_id" {
-  type = string
+  description = "Azure Subscription ID"
+  type        = string
 }
 
 variable "client_id" {
-  type = string
+  description = "Azure Client ID"
+  type        = string
 }
 
 variable "client_secret" {
-  type = string
-  sensitive = true
+  description = "Azure Client Secret"
+  type        = string
 }
 
 variable "tenant_id" {
-  type = string
+  description = "Azure Tenant ID"
+  type        = string
 }
