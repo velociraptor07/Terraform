ebb9c6a7c63f36bc2fd2b38d9e4fadcfa71ded13
velociraptor07 <sachdevyash61@gmail.com>
2025-03-26 15:49:56 -0400

    test

--- main.tf
+++ main.tf
@@ -4,12 +4,23 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "yash-rg"
+    subscription_id       = "var.subscription_id"
+    storage_account_name  = "veloyash1107"
+    container_name        = "yash01" # The container you created
+    key                   = "terraform.tfstate" # Name of the state file in the container
+  }
+}
 
+# Resource Group
 resource "azurerm_resource_group" "example" {
   name     = "yash-rg"
   location = "East US"
 }
 
+# Virtual Network
 resource "azurerm_virtual_network" "example" {
   name                = "Yash-network"
   address_space       = ["10.0.0.0/16"]
@@ -17,6 +28,7 @@ resource "azurerm_virtual_network" "example" {
   resource_group_name = azurerm_resource_group.example.name
 }
 
+# Subnet
 resource "azurerm_subnet" "example" {
   name                 = "Yash-subnet"
   resource_group_name  = azurerm_resource_group.example.name
@@ -24,6 +36,7 @@ resource "azurerm_subnet" "example" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+# Network Interface
 resource "azurerm_network_interface" "example" {
   name                = "example-nic"
   location            = azurerm_resource_group.example.location
@@ -36,6 +49,7 @@ resource "azurerm_network_interface" "example" {
   }
 }
 
+# Virtual Machine
 resource "azurerm_virtual_machine" "example" {
   name                  = "yash-vm"
   location              = azurerm_resource_group.example.location
@@ -67,4 +81,3 @@ resource "azurerm_virtual_machine" "example" {
     provision_vm_agent = true
   }
 }
-
